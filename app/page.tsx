9d5f87e44306cb37e7aae6e1d3fbad60c88a43e3
aswandeskay <aswandeskay@gmail.com>
2025-12-06 20:57:46 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Halo! Ini website Gemahripah pertamaku 🚀</h1>
+      <p>Website ini nanti akan dipakai untuk pencatatan order, stok, dan pembukuan.</p>
+    </main>
+  );
+}
